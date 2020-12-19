2111dcfeed616353f7ffab37a216c3d96c6eb4ce
Shijing Liu <brendaliu0628@gmail.com>
2020-12-19 11:44:24 -0600

    setup firebase DB and configure

---
+++ src/components/layout/Content.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Header } from './Header'
+import { Sidebar } from './Sidebar'
+
+export const Content = () => {
+ return (
+  <div>
+   <section>
+    <Sidebar />
+   </section>
+  </div>
+ )
+}
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+// get config from firebase- setting - </> at the bottom-click
+const firebaseConfig = firebase.initalizeApp({
+ apiKey: "AIzaSyB3V2LDK2Ecur3miMPvjJRA7OoB9FRZF7U",
+ authDomain: "todo-list-react-testing.firebaseapp.com",
+ databaseURL: "https://todo-list-react-testing-default-rtdb.firebaseio.com",
+ projectId: "todo-list-react-testing",
+ storageBucket: "todo-list-react-testing.appspot.com",
+ messagingSenderId: "214899241396",
+ appId: "1:214899241396:web:6cf28420498af79c0f02e1"'
+})
+
+export { firebaseConfig as firebase };
